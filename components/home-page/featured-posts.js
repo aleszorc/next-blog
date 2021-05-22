b81c67196305af24f18c220db2306912416913df
Lex-124 <ales.zorc1@gmail.com>
2021-05-22 15:20:23 +0200

    adding posts grid

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,11 @@
+import classes from './featured-posts.moduke.css';
+
+function FeauturedPosts() {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+    </section>
+  );
+}
+
+export default FeauturedPosts;
