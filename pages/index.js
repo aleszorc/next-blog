b36933e9569c9c23419938d994e8592064aff860
Lex-124 <ales.zorc1@gmail.com>
2021-05-24 19:11:11 +0200

    adding getStaticProps for home and all posts pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
+import { getFeaturedPosts } from '../lib/posts-util';
 
 const DUMMY_POSTS = [
   {
@@ -37,13 +38,23 @@ const DUMMY_POSTS = [
   },
 ];
 
-function HomePage() {
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
